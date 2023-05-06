b7d4892535430f0abe821e19a8d7f42460833c08
Franco <38784609+Altyd@users.noreply.github.com>
2023-05-06 19:09:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import openai
-import os
+import time
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = "YOUR_API_KEY"
 
-def generate_text(prompt):
+def generate_response(prompt):
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.5,
     )
-    message = response.choices[0].text.strip()
-    return message
+    return response.choices[0].text.strip()
 
-prompt = "Create a virtual girlfriend for me."
-generated_text = generate_text(prompt)
-print(generated_text)
+# Example conversation with a virtual girlfriend
+while True:
+    user_input = input("You: ")
+    response = generate_response("Virtual girlfriend: " + user_input)
+    print(response)
+    time.sleep(1)
